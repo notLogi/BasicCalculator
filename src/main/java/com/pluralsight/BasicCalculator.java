11ed52db142f9f71b115bd8e22d61dd4304a8d05
notLogi <92322066+notLogi@users.noreply.github.com>
2025-09-25 11:48:09 -0400

    Basic Calculator with only multiplication for now.

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -6,13 +6,13 @@ public class BasicCalculator {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("Enter the first number: ");
+        System.out.print("Enter the first number: ");
         double firstNumber = myScanner.nextDouble();
-        System.out.println("Enter the second number: ");
+        System.out.print("Enter the second number: ");
         double secondNumber = myScanner.nextDouble();
         myScanner.nextLine();
         System.out.println("Possible calculations:\n(A)dd\n(S)ubtract\n(M)ultiply\n(D)ivide");
-        System.out.println("Please select an option: ");
+        System.out.print("Please select an option: ");
         String operation = myScanner.nextLine();
         System.out.println(firstNumber + " * " + secondNumber + " = " + firstNumber * secondNumber);
 
